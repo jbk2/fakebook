96ba107d07a04de6e31b5fe3146e99111c68a9ac
jbk2 <james@humanbenefits.com>
2024-02-22 17:21:50 +0100

    Styling application layout with flex widths

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,23 @@
 class PostsController < ApplicationController
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 
   def index
     @posts = Post.all
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
