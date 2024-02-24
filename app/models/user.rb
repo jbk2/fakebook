a18f7bfeaab15bba55efec02d6c5e8f4be661ac4
jbk2 <james@humanbenefits.com>
2024-02-24 14:46:01 +0100

    Set up ActiveStorage for User profile_photo
    - Add has_one_attached :profile_photot to User
    - Update registrations #new & #edit to include profile_photo field
    - configure production S3 bucket region code

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,6 +18,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+  has_one_attached :profile_photo
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
