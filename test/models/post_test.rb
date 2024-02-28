2976b6092a7355009a98944556f3ce5108d0a466
jbk2 <james@humanbenefits.com>
2024-02-28 13:21:26 +0100

    Run annotate --models

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -3,7 +3,6 @@
 # Table name: posts
 #
 #  id         :bigint           not null, primary key
-#  title      :string
 #  body       :string
 #  user_id    :bigint           not null
 #  created_at :datetime         not null
