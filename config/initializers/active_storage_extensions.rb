91157aa583496d658106a0af461ab598b725ff78
jbk2 <james@humanbenefits.com>
2024-03-23 16:37:00 +0100

    Remove reference to PhotoProcessJob from ActiveStorage extensions

--- config/initializers/active_storage_extensions.rb
+++ config/initializers/active_storage_extensions.rb
@@ -1,5 +1,4 @@
 # Extend ActiveStorage models for custom associations
 Rails.application.config.to_prepare do
-  ActiveStorage::Attachment.has_one :photo_process_state, dependent: :destroy
-  ActiveStorage::Blob.has_many :attachments, class_name: 'ActiveStorage::Attachment', dependent: :nullify
+  # ActiveStorage::Blob.has_many :attachments, class_name: 'ActiveStorage::Attachment', dependent: :nullify
 end
