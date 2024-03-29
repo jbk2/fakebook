25afbd7ea9bbf6cb25027dad68992c740cc57a16
jbk2 <james@humanbenefits.com>
2024-03-29 08:10:23 +0100

    Write comment model tests

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -8,4 +8,17 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  body       :string
-#
+
+require 'rails_helper'
+
+describe Comment, type: :model do
+  describe 'associations' do
+    it { should belong_to(:user) }
+    it { should belong_to(:post) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:body) }
+    it { should validate_length_of(:body).is_at_least(3).is_at_most(250) }
+  end
+end
