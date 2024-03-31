8d37eda26e9b0b97d192f01ace53c6ced6b73a77
jbk2 <james@humanbenefits.com>
2024-03-31 12:52:23 +0200

    Test ProcessImageJob integration with users
    - update config/environments/test.rb to run activeStorage jobs
      synchronously/inline
    - add spec/integration/process_image_job_user_integration_spec.rb to test
      ProcessImageJob's integration with users' profile_photos:
      - pre-jobs completion
        - blob and attachment count increas by 1
      - post-jobs completion:
        - metadata['processed' => true]
        - byte_size is less than the original

--- spec/integration/process_image_job_user_integration_spec.rb
+++ spec/integration/process_image_job_user_integration_spec.rb
@@ -22,24 +22,23 @@
     describe '#perform' do
       it 'replaces original blob with a processed blob ' do
         original_blob_id = user.profile_photo.blob.id
-        puts "user.profile_photo attached t/f? #{user.profile_photo.attached?}"
-        puts "user.profile_photo inspect: #{user.profile_photo.inspect}"
-        puts "original_blob_id is still nill because blob's not created as user not saved yet, blob.id#nil? #{original_blob_id.nil?}"
+        # puts "user.profile_photo attached t/f? #{user.profile_photo.attached?}"
+        # puts "user.profile_photo inspect: #{user.profile_photo.inspect}"
+        # puts "original_blob_id is still nill because blob's not created as user not saved yet, blob.id#nil? #{original_blob_id.nil?}"
         
         expect { user.save }.to have_enqueued_job(ProcessImageJob)
           .and change(ActiveStorage::Attachment, :count).by(1)
           .and change(ActiveStorage::Blob, :count).by(1)
         
-        puts "\npre-processing blob byte_size= #{user.profile_photo.blob.byte_size}"
-        
+        # puts "\npre-processing blob byte_size= #{user.profile_photo.blob.byte_size}"
+        pre_jobs_byte_size = user.profile_photo.blob.byte_size
         perform_enqueued_jobs        
         user.reload
         user.profile_photo.blob.reload
-
-        puts "post-processing blob byte_size= #{user.profile_photo.blob.byte_size}\n\n"
+        # puts "post-processing blob byte_size= #{user.profile_photo.blob.byte_size}\n\n"
 
         expect(user.profile_photo.blob.metadata['processed']).to be true
-        puts "user.profile_photo.blob.inspect #{user.profile_photo.blob.inspect}"
+        expect(pre_jobs_byte_size).to be > user.profile_photo.blob.byte_size
       end
     end
 
