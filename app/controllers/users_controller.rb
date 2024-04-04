097cb35c2f7dc590a3567d291d988c3260a666ed
jbk2 <james@humanbenefits.com>
2024-04-04 17:18:17 +0100

    Further style Users#show view - 3/4 complete
    - Further work on content layout
    - Render sections as cards
    - create @common_friends instance variable

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def show
     @user = User.includes(posts: [], profile_photo_attachment: :blob).find(params[:id])
     @friends = @user.following_users + @user.followed_users
+    @common_friends = (current_user.following_users + current_user.followed_users) & @friends
   end
 
 end
