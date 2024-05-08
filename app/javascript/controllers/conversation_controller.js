cb0f9b08681e5a36a5337c82a3fac84def63e1a5
jbk2 <james@humanbenefits.com>
2024-05-08 22:04:34 +0200

    Set overflowing conversation messages to scroll bottom - Add Conversation Stimulus Controller messagesTarget - Set messagesTarget scrollTop to scrollHeight on connect

--- app/javascript/controllers/conversation_controller.js
+++ app/javascript/controllers/conversation_controller.js
@@ -3,12 +3,15 @@ import ConversationChannel from "../channels/conversation_channel";
 
 // Connects to data-controller="conversation"
 export default class extends Controller {
+  static targets = ["messages"]
   static values = { conversationId: Number }
   
   connect() {
     console.log("Conversation Stimulus Controller is now connected!")
     console.log(`The value of the conversationID from the convo card is ${this.conversationIdValue}`)
+    console.log("messages target found: ", this.messagesTarget)
 
+    this.messagesTarget.scrollTop = this.messagesTarget.scrollHeight;
     this.channel = ConversationChannel.subscribe(this.conversationIdValue);
   }
 
