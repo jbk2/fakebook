9da0c284b31142a8f7279e7b3a99c8308fea1e13
jbk2 <james@humanbenefits.com>
2024-05-28 16:26:53 +0200

    importmap update tailwindcss

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,7 +4,7 @@ pin "application"
 pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
-pin "tailwindcss", to: "https://cdn.jsdelivr.net/npm/tailwindcss@3.0.0/dist/tailwind.min.js"
+pin "tailwindcss" # @3.4.3
 pin "daisyui", to: "https://cdn.jsdelivr.net/npm/daisyui@latest/dist/full.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/actioncable", to: "actioncable.esm.js"
