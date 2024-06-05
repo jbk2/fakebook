94aa8638e3f99d6ed421244b4f0adcf56074d55c
jbk2 <james@humanbenefits.com>
2024-06-05 14:36:23 +0200

    Fully move to daisyUI CDN & add content in conversations card
    - remove or comment all reference to using daisyui via local install
    - conditionally add comment in conversations card to advise user how
      to message others if user has no conversations yet

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "tailwindcss" # @3.4.3
-# pin "daisyui", to: "https://cdn.jsdelivr.net/npm/daisyui@latest/dist/full.js" # comment out in production
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
+# pin "daisyui" # @4.11.1
