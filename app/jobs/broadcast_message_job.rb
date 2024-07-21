4ad17c50c0df315250d6ba9308121e314d4076a0
jbk2 <james@humanbenefits.com>
2024-07-21 22:02:26 +0200

    broadcast_message_job further syntax correction

--- app/jobs/broadcast_message_job.rb
+++ app/jobs/broadcast_message_job.rb
@@ -4,7 +4,7 @@ class BroadcastMessageJob < ApplicationJob
   def perform(message, sender_id, conversation_id)
     renderer = ApplicationController.renderer.new(http_host: Rails.application.credentials[:host], https: true )
     rendered_message_html = renderer.render(partial: "messages/message", locals: { message: message,
-      image_url: secure_url_for(message.user.profile_photo.blob) }
+      image_url: secure_url_for(message.user.profile_photo.blob) })
     Rails.logger.info("Generated message HTML: #{rendered_message_html}")
     # image_url = Rails.application.routes.url_helpers.rails_blob_url(message.user.profile_photo, host: '13.39.87.140:3000')
     # image_url = Rails.application.routes.url_helpers.rails_blob_url(message.user.profile_photo, host: 'fakebook.bibble.com')
