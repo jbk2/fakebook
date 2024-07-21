a213ab6a227bf7c9eb01a26cc986739648d89dad
jbk2 <james@humanbenefits.com>
2024-07-21 16:24:03 +0200

    set renderer in broadcast_message_job to https

--- app/jobs/broadcast_message_job.rb
+++ app/jobs/broadcast_message_job.rb
@@ -2,7 +2,7 @@ class BroadcastMessageJob < ApplicationJob
   queue_as :default
 
   def perform(message, sender_id, conversation_id)
-    renderer = ApplicationController.renderer.new(http_host: Rails.application.credentials[:host], https: false )
+    renderer = ApplicationController.renderer.new(http_host: Rails.application.credentials[:host], https: true )
     rendered_message_html = renderer.render(partial: "messages/message", locals: { message: message })
     # image_url = Rails.application.routes.url_helpers.rails_blob_url(message.user.profile_photo, host: '13.39.87.140:3000')
     sender = User.find(sender_id)
