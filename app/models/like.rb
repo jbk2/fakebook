c4d6181a83f7e184eac92dc871be1de407e3a713
jbk2 <james@humanbenefits.com>
2024-07-27 21:23:33 +0200

    Refine Like error messages
    - Stop Rails prepending Like error mssgs with validation object name

--- app/models/like.rb
+++ app/models/like.rb
@@ -12,12 +12,20 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post, counter_cache: true
 
-  validates :user_id, uniqueness: { scope: :post_id, message: "you've already liked this post"}
+  # validates :user_id, uniqueness: { scope: :post_id, message: "you've already liked this post"}
+  validate :unique_like_per_user_post
   validate :not_own_post
 
   private
+  def unique_like_per_user_post
+    # Check if a like already exists with the same user_id and post_id
+    if Like.exists?(user_id: user_id, post_id: post_id)
+      errors.add(:base, "You've already liked this post")
+    end
+  end
+
   def not_own_post
-    errors.add(:post_id, "you can't like your own post") if user_id == post.user_id
+    errors.add(:base, "You can't like your own post") if user_id == post.user_id
   end
 
 end
