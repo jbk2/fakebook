45ac4eb5d67011c957050960eba4b58e507a18ba
jbk2 <james@humanbenefits.com>
2024-08-06 20:49:06 +0200

    Style log output in remaining un-log-styled  Stimulus Controllers

--- app/javascript/controllers/comment_controller.js
+++ app/javascript/controllers/comment_controller.js
@@ -5,13 +5,13 @@ export default class extends Controller {
   static targets = [ "form" ]
   connect() { 
     this.formTarget.classList.add("hidden");
-    console.log("comment_controller Stimulus controller is now connected");
-    console.log("comment form taget found", this.formTarget)
+    console.log(`%cCommentStimulus=>%c connected\n
+      comment FormTarget found; ${this.formTarget}`, "color: blue; font-weight: bold;", "");
   }
 
   toggle(event) {
     event.preventDefault();
-    console.log("toggle button clicked");
+    console.log("%cCommentStimulus=>%c toggle Comment form button clicked", "color: blue; font-weight: bold;", "");
     this.formTarget.classList.toggle("hidden");
   }
 
--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -3,7 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="flash"
 export default class extends Controller {
   connect() {
-    console.log("Flash controller connected")
+    console.log("%cFlashStimulus=>%c connected", "color: blue; font-weight: bold;", "")
     setTimeout(() => {
       this.element.remove();
     }, 2500);
