4c9f6066037f636fdb6fd43f39d36708d5ebc7aa
jbk2 <james@humanbenefits.com>
2024-08-08 16:16:08 +0200

    Fix theme toggle styling bug
    - remove "@tailwindcss/forms" from package.json & tailwind.confog.js as
      it dupliactes what daisy-ui already pulls in, clobber and precompile
      and styling bug fixed.
      - did need some text-field padding left adjustments afterwards

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
+    // require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
