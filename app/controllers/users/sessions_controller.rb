ead7111dedc6772a4fe6c806ac201b0074380a1a
jbk2 <james@humanbenefits.com>
2024-08-12 15:08:59 +0200

    ensure reset_session when rate_limit triggered

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,8 +1,11 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  rate_limit to: 10, within: 3.minutes, only: :create,
-    with: -> { redirect_to new_user_session_path, alert: "Login rate limit exceeded - Try again later." }
+  rate_limit to: 1, within: 1.minute, only: :create,
+    with: -> {
+      reset_session
+      redirect_to new_user_session_path, alert: "Login rate limit exceeded - Try again later."
+    }
       
   # before_action :configure_sign_in_params, only: [:create]
 
