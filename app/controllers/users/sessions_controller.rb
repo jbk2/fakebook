a7a69b2b516a3e57845ebfec0e8202ac5b1f9c58
jbk2 <james@humanbenefits.com>
2024-08-12 15:21:40 +0200

    Set sessions#create rate_limit back to 10 per 3mins

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::SessionsController < Devise::SessionsController
-  rate_limit to: 1, within: 1.minute, only: :create,
+  rate_limit to: 10, within: 3.minutes, only: :create,
     with: -> {
       reset_session
       redirect_to new_user_session_path, alert: "Login rate limit exceeded - Try again later."
