9ce366a0afc7c3ae54f5d96948f855b95a06e815
jbk2 <james@humanbenefits.com>
2025-08-17 12:38:49 +0200

    Add session debug logging to ApplicationController, + markup typo in Navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,10 @@ class ApplicationController < ActionController::Base
   include Pagy::Backend
   helper DeviseHelpers # Include the DeviseHelpers 'controller & action identification' module
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!#, :debug_session
   before_action :set_conversations_and_conversation, if: -> { user_signed_in? }
 
   private
-
   def set_conversations_and_conversation
     set_conversations
     set_conversation
@@ -22,5 +21,10 @@ class ApplicationController < ActionController::Base
       @conversation = Conversation.includes(participant_one: [profile_photo_attachment: :blob],
         participant_two: [profile_photo_attachment: :blob]).find(session[:active_conversation_id])
     end
+  end
+
+  def debug_session
+    Rails.logger.info "Session size: #{session.to_hash.to_s.bytesize} bytes"
+    Rails.logger.info "Session keys: #{session.to_hash.keys.inspect}"
   end
 end
